0802eb98c1ec56ee2f7735e001951cae592da135
farhan.huda <farhan.huda@pactindo.com>
2023-05-22 09:15:42 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// const coffee = require('./lib/coffee');
+// console.log(coffee);
+
+// const cpuInformation = process.memoryUsage();
+// console.log(cpuInformation);
+// /* output
+// {
+//   rss: 14569472,
+//   heapTotal: 2654208,
+//   heapUsed: 1788896,
+//   external: 855681,
+//   arrayBuffers: 9898
+// }
+// */
+// const firstName = process.argv[2];
+// const lastName = process.argv[3];
+// console.log(`Hello ${firstName} ${lastName}`);
